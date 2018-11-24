b97c5d3a0cac9f0e70063be52e7ca7e441dd52e5
András Jankó <dandrid2@gmail.com>
2018-11-24 19:27:12 +0100

    Added initial game logic

--- HexaColor/Model/Model.cs
+++ HexaColor/Model/Model.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -61,8 +61,8 @@ namespace HexaColor.Model
         public override int GetHashCode()
         {
             var hashCode = 484048395;
-            hashCode = hashCode * -1521134295 + XCooridnate.GetHashCode();
-            hashCode = hashCode * -1521134295 + YCooridnate.GetHashCode();
+            hashCode = hashCode * -1521134295 + rowCooridnate.GetHashCode();
+            hashCode = hashCode * -1521134295 + columnCooridnate.GetHashCode();
             return hashCode;
         }
     }
@@ -100,6 +100,10 @@ namespace HexaColor.Model
         private int usedColors;
         private int mapSize;
         public Dictionary<Position, Cell> cells { get; internal set; }
+
+        /**
+         * Create a map with the given parameters, and initialize with random colors 
+         */
         public MapLayout(int rows, int columns, int usedColors)
         {
             
@@ -153,11 +157,17 @@ namespace HexaColor.Model
                 var actualNeighbourCells = new List<Cell>();
                 foreach (var possiblePosition in possibleNeighbourPositions)
                 {
-                    if(isValidPosition(possiblePosition))
+                    if (isValidPosition(possiblePosition))
                     {
                         Cell neighbourCell;
-                        cells.TryGetValue(possiblePosition, out neighbourCell);
-                        actualNeighbourCells.Add(neighbourCell);
+                        if (cells.TryGetValue(possiblePosition, out neighbourCell))
+                        {
+                            actualNeighbourCells.Add(neighbourCell);
+                        }
+                        else
+                        {
+                            throw new InvalidOperationException(string.Format("Invalid state, cell not exists, row: {0}, column: {1}", possiblePosition.rowCooridnate, possiblePosition.columnCooridnate));
+                        }
                     }
                 }
                 cell.neighbourCells = actualNeighbourCells;
@@ -191,8 +201,184 @@ namespace HexaColor.Model
             if (cells.TryGetValue(position, out cell))
             {
                 cell.changeContinousColors(color);
-            }       
+            }
+            else
+            {
+                throw new ArgumentException("Position not found in the game!");
+            }
         }
 
+        public Queue<Position> getPlayerStartingPositions(int playerNumber)
+        {
+            Queue<Position> startingPositions = new Queue<Position>();
+            if (playerNumber == 2)
+            {
+                startingPositions.Enqueue(new Position(0, 0));
+                startingPositions.Enqueue(new Position(mapSize - 1, mapSize - 1));
+            }
+            else if (playerNumber == 4)
+            {
+                startingPositions.Enqueue(new Position(0, 0));
+                startingPositions.Enqueue(new Position(0, mapSize - 1));
+                startingPositions.Enqueue(new Position(mapSize - 1, 0));
+                startingPositions.Enqueue(new Position(mapSize - 1, mapSize - 1));
+            }
+            else if (playerNumber == 8)
+            {
+                int middleIndex = mapSize / 2;
+
+                // Upper 2
+                startingPositions.Enqueue(new Position(0, middleIndex - 1));
+                startingPositions.Enqueue(new Position(0, middleIndex + 1));
+
+                // Down 2
+                startingPositions.Enqueue(new Position(mapSize - 1, middleIndex - 1));
+                startingPositions.Enqueue(new Position(mapSize - 1, middleIndex + 1));
+
+                // Left 2
+                startingPositions.Enqueue(new Position(middleIndex - 1, 0));
+                startingPositions.Enqueue(new Position(middleIndex + 1, 0));
+
+                // Right 2
+                startingPositions.Enqueue(new Position(middleIndex - 1, mapSize - 1));
+                startingPositions.Enqueue(new Position(middleIndex + 1, mapSize - 1));
+            }
+            else
+            {
+                throw new InvalidOperationException(string.Format("Invalid state, wrong number of players: {0}", playerNumber));
+            }
+            return startingPositions;
+        }
+    }
+
+    public class Player
+    {
+        public readonly Position startingPosition;
+        public string name { get; set; }
+        public int points { get; set; }
+
+        public Player(Position startingPosition, string name)
+        {
+            this.startingPosition = startingPosition;
+            this.name = name;
+            this.points = 0;
+        }
+    }
+
+    public class Game
+    {
+        private MapLayout mapLayout;
+        private List<Player> players;
+        private Queue<Position> availableStartingPositions;
+
+        public Game(int playerNumber, int usedColors, int rows, int columns)
+        {
+            if (playerNumber != 2 || playerNumber != 4 || playerNumber != 8)
+            {
+                throw new ArgumentException("Player size must be 2, 4 or 8.");
+            }
+            if (usedColors < playerNumber + 2 || 20 < usedColors)
+            {
+                throw new ArgumentException("Invalid color number!");
+            }
+            players = new List<Player>();
+
+            mapLayout = new MapLayout(rows, columns, usedColors);
+            availableStartingPositions = mapLayout.getPlayerStartingPositions(playerNumber);
+        }
+
+        public Player addNewPlayer(string name = "default player name")
+        {
+            if (availableStartingPositions.Count != 0)
+            {
+                Position startingPosition = availableStartingPositions.Dequeue();
+                Player newPlayer = new Player(startingPosition, name);
+                players.Add(newPlayer);
+                return newPlayer;
+            }
+            throw new InvalidOperationException(string.Format("All player slots are filled! Current players: {0}", players.Count));
+        }
+
+        public bool isGameFull()
+        {
+            return availableStartingPositions.Count == 0;
+        }
+
+        public void handleChanges(GameChange change)
+        {
+            if(change is ColorChange)
+            {
+                ColorChange colorChange = (ColorChange) change;
+                mapLayout.changeContinousColors(colorChange.player.startingPosition, colorChange.newColor);
+
+                // Calculate next player
+                int nextPlayerIndex = (players.IndexOf(colorChange.player) + 1) % players.Count;
+                Player nextPlayer = players.ElementAt(nextPlayerIndex);
+
+                throw new NotImplementedException(); // TODO Send next player to client
+            }
+
+            if (isGameWon())
+            {
+                calculatePoints();
+                throw new NotImplementedException(); // TODO Handle the game won, send to clients
+            }
+        }
+        
+        public bool isGameWon()
+        {
+            ISet<Color> differentColorsInGame = new HashSet<Color>();
+            foreach (var pair in mapLayout.cells)
+            {
+                Cell cell = pair.Value;
+                differentColorsInGame.Add(cell.color);
+            }
+            return differentColorsInGame.Count == players.Count;
+        }
+
+        public void calculatePoints()
+        {
+            Dictionary<Color, Player> colorToPlayer = new Dictionary<Color, Player>();
+            foreach(Player player in players)
+            {
+                Cell playerCell;
+                if(mapLayout.cells.TryGetValue(player.startingPosition, out playerCell))
+                {
+                    colorToPlayer.Add(playerCell.color, player);
+                }
+                else
+                {
+                    throw new InvalidOperationException("Invalid state, player must have a starting position!");
+                }
+            }
+            
+            foreach(var pair in mapLayout.cells)
+            {
+                Cell cell = pair.Value;
+                Player player;
+                if (colorToPlayer.TryGetValue(cell.color, out player))
+                {
+                    player.points++;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Invalid state, game is not won!");
+                }
+            }
+        }
+    }
+
+    public interface GameChange { }
+
+    public class ColorChange : GameChange
+    {
+        public readonly Player player; // This can also be an ID if needed
+        public readonly Color newColor;
+
+        public ColorChange(Player player, Color newColor)
+        {
+            this.player = player;
+            this.newColor = newColor;
+        }
     }
 }
