ba2fc773b3f5a39ad3ad72a875f88691b5436afe
Nyikovics Máté <nyikovicsmate@gmail.com>
2018-11-25 15:14:21 +0100

    Removed color picker from game creation.

--- HexaColor.Client/Views/CreatingControlsView.xaml.cs
+++ HexaColor.Client/Views/CreatingControlsView.xaml.cs
@@ -42,36 +42,36 @@ namespace HexaColor.Client.Views
         {
             InitializeComponent();
 
-            chosenColors = new List<Model.Color>();
-            InitColors();
+            //chosenColors = new List<Model.Color>();
+            //InitColors();
         }
 
-        private void InitColors()
-        {
-            foreach(var item in ColorMap.Items)
-            {
-                Button btn = new Button();
-                btn.Background = item.Value;
-                btn.Style = FindResource("NotPressedColorBtnStyle") as Style;
-                btn.Click += ColorBtn_Click;
-                ColorsPanel.Children.Add(btn);
-            }
-        }
+        //private void InitColors()
+        //{
+        //    foreach(var item in ColorMap.Items)
+        //    {
+        //        Button btn = new Button();
+        //        btn.Background = item.Value;
+        //        btn.Style = FindResource("NotPressedColorBtnStyle") as Style;
+        //        btn.Click += ColorBtn_Click;
+        //        ColorsPanel.Children.Add(btn);
+        //    }
+        //}
 
-        private void ColorBtn_Click(object sender, RoutedEventArgs e)
-        {
-            Button btn = sender as Button;
-            Model.Color btnColor = ColorMap.Items.Where(c => c.Value.Equals(btn.Background)).First().Key;
-            if (chosenColors.Contains(btnColor))
-            {
-                chosenColors.Remove(btnColor);
-                btn.Style = FindResource("NotPressedColorBtnStyle") as Style;
-            } else
-            {
-                chosenColors.Add(btnColor);
-                btn.Style = FindResource("PressedColorBtnStyle") as Style;
-            }
-        }
+        //private void ColorBtn_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Button btn = sender as Button;
+        //    Model.Color btnColor = ColorMap.Items.Where(c => c.Value.Equals(btn.Background)).First().Key;
+        //    if (chosenColors.Contains(btnColor))
+        //    {
+        //        chosenColors.Remove(btnColor);
+        //        btn.Style = FindResource("NotPressedColorBtnStyle") as Style;
+        //    } else
+        //    {
+        //        chosenColors.Add(btnColor);
+        //        btn.Style = FindResource("PressedColorBtnStyle") as Style;
+        //    }
+        //}
 
         private void CancelBtn_Click(object sender, RoutedEventArgs e)
         {
@@ -80,7 +80,11 @@ namespace HexaColor.Client.Views
 
         private void OkBtn_Click(object sender, RoutedEventArgs e)
         {
+            string name = NameBox.Text;
+            int.TryParse(ColorBox.Text, out int colorCnt);
+            int.TryParse(SizeBox.Text, out int size);
             LeftPanelDataContext = new MapLayoutModel();
+            MessageBox.Show(String.Format("Initialized Name: {0}, Color count: {1}, Map size: {2}", name, colorCnt, size));
         }
     }
 }
