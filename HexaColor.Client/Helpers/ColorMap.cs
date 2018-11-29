94cb465cffd2545befbfc37e6a836463a4c569ba
András Jankó <dandrid2@gmail.com>
2018-11-29 03:50:13 +0100

    Updated color map, so the colors can be easier to distinguish

--- HexaColor.Client/Helpers/ColorMap.cs
+++ HexaColor.Client/Helpers/ColorMap.cs
@@ -14,26 +14,26 @@ namespace HexaColor.Client.Helpers
         static ColorMap()
         {
             Items = new Dictionary<Model.Color, SolidColorBrush>();
-            Items.Add(Model.Color.COLOR_1, Brushes.Aquamarine);
-            Items.Add(Model.Color.COLOR_2, Brushes.Azure);
-            Items.Add(Model.Color.COLOR_3, Brushes.Bisque);
-            Items.Add(Model.Color.COLOR_4, Brushes.Coral);
-            Items.Add(Model.Color.COLOR_5, Brushes.Cyan);
-            Items.Add(Model.Color.COLOR_6, Brushes.DarkGoldenrod);
-            Items.Add(Model.Color.COLOR_7, Brushes.DarkOrange);
-            Items.Add(Model.Color.COLOR_8, Brushes.Firebrick);
-            Items.Add(Model.Color.COLOR_9, Brushes.GhostWhite);
-            Items.Add(Model.Color.COLOR_10, Brushes.Gold);
-            Items.Add(Model.Color.COLOR_11, Brushes.HotPink);
-            Items.Add(Model.Color.COLOR_12, Brushes.IndianRed);
-            Items.Add(Model.Color.COLOR_13, Brushes.Khaki);
-            Items.Add(Model.Color.COLOR_14, Brushes.Lavender);
-            Items.Add(Model.Color.COLOR_15, Brushes.Magenta);
-            Items.Add(Model.Color.COLOR_16, Brushes.Navy);
-            Items.Add(Model.Color.COLOR_17, Brushes.OliveDrab);
-            Items.Add(Model.Color.COLOR_18, Brushes.PaleVioletRed);
-            Items.Add(Model.Color.COLOR_19, Brushes.PeachPuff);
-            Items.Add(Model.Color.COLOR_20, Brushes.Salmon);
+            Items.Add(Model.Color.COLOR_1, Brushes.Red);
+            Items.Add(Model.Color.COLOR_2, Brushes.LimeGreen);
+            Items.Add(Model.Color.COLOR_3, Brushes.MediumBlue);
+            Items.Add(Model.Color.COLOR_4, Brushes.BlueViolet);
+            Items.Add(Model.Color.COLOR_5, Brushes.LightSkyBlue);
+            Items.Add(Model.Color.COLOR_6, Brushes.LightSlateGray);
+            Items.Add(Model.Color.COLOR_7, Brushes.Olive);
+            Items.Add(Model.Color.COLOR_8, Brushes.Crimson);
+            Items.Add(Model.Color.COLOR_9, Brushes.Tomato);
+            Items.Add(Model.Color.COLOR_10, Brushes.Gainsboro);
+            Items.Add(Model.Color.COLOR_11, Brushes.Gold);
+            Items.Add(Model.Color.COLOR_12, Brushes.Yellow);
+            Items.Add(Model.Color.COLOR_13, Brushes.Brown);
+            Items.Add(Model.Color.COLOR_14, Brushes.DarkTurquoise);
+            Items.Add(Model.Color.COLOR_15, Brushes.GreenYellow);
+            Items.Add(Model.Color.COLOR_16, Brushes.Magenta);
+            Items.Add(Model.Color.COLOR_17, Brushes.Khaki);
+            Items.Add(Model.Color.COLOR_18, Brushes.Orange);
+            Items.Add(Model.Color.COLOR_19, Brushes.Violet);
+            Items.Add(Model.Color.COLOR_20, Brushes.RosyBrown);
         }
 
     }
