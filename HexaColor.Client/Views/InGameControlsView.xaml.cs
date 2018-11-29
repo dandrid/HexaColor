4489d74ce83e6118bf33d8114d5bb7a01d476a0f
Nyikovics Máté <nyikovicsmate@gmail.com>
2018-11-29 01:48:01 +0100

    Filled in the InGameControlsView.

--- HexaColor.Client/Views/InGameControlsView.xaml.cs
+++ HexaColor.Client/Views/InGameControlsView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HexaColor.Client.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,64 @@ namespace HexaColor.Client.Views
     /// </summary>
     public partial class InGameControlsView : UserControl
     {
+        private Button[] colorButtons;
+
         public InGameControlsView()
         {
             InitializeComponent();
+            InitColors();
+        }
+
+        private void InitColors()
+        {
+            colorButtons = (ChangeBtn.Parent as Grid).Children.OfType<Button>().Where(b => Grid.GetRow(b) > 0).ToArray();
+            if(colorButtons.Count() != ColorMap.Items.Count)
+            {
+                throw new Exception("Model-view mismatch. There are less or more model elements than visual elements.");
+            }
+            DisableColorButtons();
+        }
+
+        private void DisableColorButtons()
+        {
+            for (int i = 0; i < colorButtons.Count(); i++)
+            {
+                Color origColor = ColorMap.Items.ElementAt(i).Value.Color;
+                float factor = 0.4f;
+                Color newColor = Color.FromScRgb(origColor.ScA * factor, origColor.ScR * factor, origColor.ScG * factor, origColor.ScB * factor);
+                colorButtons[i].Background = new SolidColorBrush(newColor);
+                colorButtons[i].IsEnabled = false;
+            }
+        }
+
+        private void EnableleColorButtons()
+        {
+            for (int i = 0; i < colorButtons.Count(); i++)
+            {
+                colorButtons[i].Background = ColorMap.Items.ElementAt(i).Value;
+                colorButtons[i].IsEnabled = true;
+            }
+        }
+
+        private void ChangeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO
+            MessageBox.Show("Change!");
+            DisableColorButtons();
+        }
+
+        private void SkipBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO
+            EnableleColorButtons();
+            MessageBox.Show("Skip");
+        }
+
+        private void ColorBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            ChangeBtn.Background = btn.Background;
+            ChangeBtn.IsEnabled = true;
         }
     }
 }
--- HexaColor.Client/Views/StartingControlsView.xaml.cs
+++ HexaColor.Client/Views/StartingControlsView.xaml.cs
@@ -43,7 +43,8 @@ namespace HexaColor.Client.Views
 
         private void ConnectBtn_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Connect");
+            //MessageBox.Show("Connect");
+            RightPanelDataContext = new InGameControlsModel();
         }
     }
 }
